8892a94feef10f171265301f600fd62280c8a621
Mizuki <mizuki255@gmail.com>
2024-08-14 12:00:21 +0900

    chart added

--- src/mylib/Common.py
+++ src/mylib/Common.py
@@ -3,11 +3,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import yfinance as yf
+import mplfinance as mpf
+
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-def GetData(ticker):
-    data = yf.download(ticker , period='5d', interval = "1d")
-    df = data
-    df = df.drop("Volume", axis=1)
-    
-    return df
+figSizeHeight = 3
+figSizeWidth = 4
+
+
+class data:
+    dataframe = 0
+
+def GetData(ticker, data):
+    rawData = yf.download(ticker , period='1y', interval = "1d")
+    data.dataframe = rawData
+    print(data.dataframe)
+
+def DrawPlot(data, frame):    
+    data.dataframe = data.dataframe.drop("Volume", axis=1)
+    fig = plt.figure(figsize=(figSizeWidth, figSizeHeight))
+    ax = fig.add_subplot(1, 1, 1)
+    data.dataframe.plot(ax=ax)
+    canvas = FigureCanvasTkAgg(fig, frame)  # Generate canvas instance, Embedding fig in frame
+    canvas.get_tk_widget().pack()
+    canvas._tkcanvas.pack()
+
+def DrawCandle(data, frame):
+    fig, axlist = mpf.plot(data.dataframe, type="candle", volume=True,figratio=(10,5), returnfig=True)
+    fig.set_size_inches(figSizeWidth, figSizeHeight)
+    canvas = FigureCanvasTkAgg(fig, frame)  # Generate canvas instance, Embedding fig in frame
+    canvas.get_tk_widget().pack()
+    canvas._tkcanvas.pack()
